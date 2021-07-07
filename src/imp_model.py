2e5b60f93b2ca86fb751cb5f9303ef3c0fb62fe4
MaazKhaleeq <mkhaleeq.bee17seecs@seecs.edu.pk>
2021-07-07 21:02:39 +0500

    rev

--- src/imp_model.py
+++ src/imp_model.py
@@ -88,10 +88,12 @@ class Modal:
         #temporalCN = TCN(output_size, num_channels=num_hidden)
         temporalCN = TCN(nb_filters=num_hidden, return_sequences=True)
         temp_out = temporalCN(tcn_in3d)
+
+        concat = tf.expand_dims(tf.concat([temp_out,temp_out],2),2)
         
         
         kernel = tf.Variable(tf.random.truncated_normal([1, 1, num_hidden, len(self.char_list) + 1], stddev=0.1))
-        self.rnn_out_3d = tf.squeeze(tf.nn.atrous_conv2d(value=temp_out, filters=kernel, rate=1, padding='SAME'),
+        self.rnn_out_3d = tf.squeeze(tf.nn.atrous_conv2d(value=concat, filters=kernel, rate=1, padding='SAME'),
                                      axis=[2])
 
     def setup_ctc(self) -> None:
